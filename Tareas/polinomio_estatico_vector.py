80318fe5304e9c797f93d2b4cbf6d4542cc4f1b4
aReMCi <117370704+aReMCi@users.noreply.github.com>
2025-04-17 13:03:21 +0000

    Act pol vector

--- Tareas/polinomio_estatico_vector.py
+++ Tareas/polinomio_estatico_vector.py
@@ -5,39 +5,43 @@ class monomio:
         self.exponente = exponente
 
 class polinomio:
-    def __init__(self, dimension =10, monomios = None):
-        if not isinstance(monomios, list):
-            raise TypeError("Los monomios deben ser una lista")
-        if dimension < 10:
-            raise ValueError("El polinomio no puede tener menos de 10 monomios")
+    def __init__(self, dimension =10, polinomio = None):
+        if not isinstance(polinomio, list):
+            raise TypeError("El polinomio debe ser una lista")
+        if dimension > 10:
+            raise ValueError("El polinomio no puede tener más de 10 términos")
         self.dimension = dimension
-        self.monomios = monomios[:dimension]
-
-    def Agregar_termino(self, nuevo_monomio):
-        if self.dimension == len(self.monomios):
-            raise ValueError("El polinomio ha alcanzado su dimension maxima")
+        self.polinomio = polinomio[:dimension]
+    
+    def set_termino(self, pos, nuevo_monomio):
+        pos= pos-1
         if not isinstance(nuevo_monomio, monomio):
             raise TypeError("El monomio debe ser de tipo monomio")
-        
-        self.monomios.append(nuevo_monomio)
+        if pos <= self.dimension:
+            self.polinomio[pos] = nuevo_monomio
+        else:   
+            raise IndexError("Posición fuera de rango")
 
     def Get_termino(self,pos):
-        pos = pos-1
-        if pos < 0 or pos >= len(self.monomios):
+        if pos < 0 or pos >= len(self.polinomio):
             raise IndexError("Posición fuera de rango")
-        return self.monomios[pos]    
-    
-    def __str__(self):
-        return " + ".join([f"{monomio.coeficiente}{monomio.variable}^{monomio.exponente}" for monomio in self.monomios])
+        return self.polinomio[pos]    
+
+    def __str__ (self):
+        resultado = ""
+        for i in range(len(self.polinomio)):
+            if self.polinomio[i] is not None:
+                resultado += f"{self.polinomio[i].coeficiente}{self.polinomio[i].variable}^{self.polinomio[i].exponente} + "
+        return resultado[:-3] #Elimina el último " + "
 
 #Ejemplo
 monomio1 = monomio(2, "x", 2)
 monomio2 = monomio(3, "x", 2)
+monomio3 = monomio(4, "x", 1)
 
 polinomio1 = polinomio(10, [])
-polinomio1.Agregar_termino(monomio1)
-polinomio1.Agregar_termino(monomio2)
-
-term = polinomio1.Get_termino(2)
-print(f"El primer monomio es: {term.coeficiente}{term.variable}^{term.exponente}")
-
+polinomio1.set_termino(1, monomio1)
+polinomio1.set_termino(2, monomio2)
+print(polinomio1)
+term = polinomio1.Get_termino(1)
+print(f"EL termino {term.coeficiente}{term.variable}^{term.exponente}")
